c273545e2cdd8a532dc30aa7aaf07782b4c70e32
lcw <skcjddn@gmail.com>
2024-09-30 23:13:58 +0900

    feat: constants 파일 추가

---
+++ lib/utils/constants.dart
@@ -0,0 +1,8 @@
+class Constants {
+  // 네비게이션 경로
+  static const String splashNavigationRoute = '/splash';
+  static const String bottomNavigationRoute = '/bottom_navigation';
+
+// 이미지 경로
+  static const String splashBackgroundImage = 'assets/backgrounds/background_splash.png';
+}
