3eef223417ca8b51b978613561e03ad666794493
lcw <skcjddn@gmail.com>
2024-10-12 23:50:25 +0900

    refactor: router viewmodel사용

--- lib/router/router.dart
+++ lib/router/router.dart
@@ -1,12 +1,13 @@
 import 'package:charge_mate/presentation/login/login_screen.dart';
+import 'package:charge_mate/presentation/login/login_view_model.dart';
 import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
 
 import '../presentation/bottom_navigation/bottom_navigation_screen.dart';
 import '../presentation/splash/splash_screen.dart';
 import '../utils/constants.dart';
 
-final router = GoRouter(
-    initialLocation: Constants.splashNavigationRoute, routes: [
+final router = GoRouter(initialLocation: Constants.splashNavigationRoute, routes: [
   GoRoute(
       path: Constants.splashNavigationRoute,
       builder: (context, state) {
@@ -20,6 +21,9 @@ final router = GoRouter(
   GoRoute(
       path: Constants.loginRoute,
       builder: (context, state) {
-        return const LoginScreen();
+        return ChangeNotifierProvider(
+          create: (_) => LoginViewModel(),
+          child: const LoginScreen(),
+        );
       }),
 ]);
